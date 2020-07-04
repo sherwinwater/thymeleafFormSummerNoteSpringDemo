461a5dd090e7e8dffc99d707920351e2f6f58422
sherwin <sherwin@liMac.local>
2020-07-04 10:08:08 -0400

    first commit

---
+++ src/main/java/bootstrap/model/Post.java
@@ -0,0 +1,14 @@
+package bootstrap.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Post {
+
+    private String title;
+    private String content;
+}
